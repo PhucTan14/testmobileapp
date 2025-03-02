59c9c8018b15f129807cb390ec5d72cebe9e4dc7
duyan77 <fklt0961@gmail.com>
2025-03-02 11:10:07 +0700

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,14 +84,18 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import pymysql
 
+pymysql.install_as_MySQLdb()
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mobileapp',
+		'USER': 'root',
+		'PASSWORD': 'Phuc14kute@',
+		'HOST': ''  # mặc định localhos
 	}
 }
-
 
 # RDS database configuration settings
 # DATABASES = {
